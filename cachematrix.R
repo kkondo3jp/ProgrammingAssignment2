d2e19160e0e3d269afd351b4ea6b0d7263cb735e
Kazz Kondo <kkondo3jp@users.noreply.github.com>
2014-08-25 17:48:45 +0900

    indent corrected

--- cachematrix.R
+++ cachematrix.R
@@ -33,14 +33,14 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-m <- x$getinv()
-if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-}
-data <- x$get()
-m <- solve(data, ...)
-x$setinv(m)
-m
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 
 }
